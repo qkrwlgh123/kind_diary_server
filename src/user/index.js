f19b8f7dd36e254438468a2c97a8458ade10cf46
qkrwlgh123 <jiho1024@naver.com>
2024-07-15 10:16:03 +0900

    패스워드 변경 API index.js 반영

--- src/user/index.js
+++ src/user/index.js
@@ -2,10 +2,12 @@ import express from "express";
 import { handleCreateUser } from "./create.js";
 import { handleLoginUser } from "./login.js";
 import { handleValidateToken } from "./validate.js";
+import { handleChangePassword } from "./changePw.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/create", handleCreateUser);
+userRouter.put("/changepw", handleChangePassword);
 userRouter.post("/login", handleLoginUser);
 userRouter.post("/validate", handleValidateToken);
 

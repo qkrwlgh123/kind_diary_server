f622ffeae1fecf047897ac13f2259d263c5be91a
qkrwlgh123 <jiho1024@naver.com>
2024-08-08 20:35:49 +0900

    목표 샥제 API request 형태 수정

--- src/object/delete.js
+++ src/object/delete.js
@@ -5,7 +5,7 @@ const prisma = new PrismaClient();
 /** 목표 삭제 API */
 export const handleDeleteObject = async (req, res) => {
   try {
-    const bodyInfo = req.body.data;
+    const bodyInfo = req.body;
 
     const objectId = bodyInfo.objectId;
 
